8c12d063211098d33cd115e9902a8f8aeca41814
yifan117 <yifan.qiu17@gmail.com>
2023-03-05 15:00:35 +1030

    completed, should look for more optimal space solution

--- partition_labels.cpp
+++ partition_labels.cpp
@@ -3,26 +3,26 @@
 #include <vector>
 #include <unordered_map>
 
-std::vector<int> partitionLabels(std::string s) {
-    std::unordered_map<char, int> active;
-    std::unordered_map<char, int> map;
+std::vector<int> partitionLabels(std::string s) { // O(N) time, O(N) space
+    std::unordered_map<char, int> active; // keeps track of active values
+    std::unordered_map<char, int> map; // keeps track of all values
     int count = 0;
     std::vector<int> ans;
 
-    for (char val : s) {
+    for (char val : s) { // populate total map
         map[val]++;
     }
 
     for (char val : s) {
-        active[val]++;
+        active[val]++; // populate active
         count++;
         
-        if (active[val] == map[val]) {
-            map.erase(val);
-            active.erase(val);
+        if (active[val] == map[val]) { // if active = map, meaning all occurrences of a single character have been counted
+            map.erase(val); // erase from map
+            active.erase(val); // erase from active
 
-            if (active.size() == 0) {
-                ans.push_back(count);
+            if (active.size() == 0) { // if active is empty (meaning no values are current active, ie a partition can be made)
+                ans.push_back(count); // add count to answer, reset count
                 count = 0;
             }
         }
