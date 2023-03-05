ce89a7a6ebf04b58ca9be18fabff3a3e2703c797
yifan117 <yifan.qiu17@gmail.com>
2023-03-05 14:49:21 +1030

    completed

--- jump_game_iv.cpp
+++ jump_game_iv.cpp
@@ -6,14 +6,14 @@
 // https://leetcode.com/problems/jump-game-iv/solutions/1690813/best-explanation-ever-possible-for-this-question/
 
 int minJumps(std::vector<int>& arr) {
-    int n = arr.size();
-    if (n == 1) return 0;
+    int n = arr.size(); // get size of vec
+    if (n == 1) return 0; // base case
 
     std::unordered_map<int, std::vector<int>> mp;
     int step = 0;
 
     for (int i = 0; i < n; i++) {
-        mp[arr[i]].push_back(i);
+        mp[arr[i]].push_back(i); // sets key of map to arr[i], and the value is a vector of indexes
     }
 
     std::queue<int> q;
@@ -21,31 +21,31 @@ int minJumps(std::vector<int>& arr) {
     q.push(0);
 
     while (!q.empty()) {
-        step++;
-        int size = q.size();
+        step++; // increments number of steps to reach end
+        int size = q.size(); // gets size of queue
         for (int i = 0; i < size; i++) {
-            int j = q.front();
+            int current = q.front();
             q.pop();
 
-            if ((j - 1 >= 0) && (mp.find(arr[j-1]) != mp.end())) {
-                q.push(j - 1);
+            if ((current - 1 >= 0) && (mp.find(arr[current-1]) != mp.end())) { // if index - 1 is not outside of vector and the value is in the map, push the value to queue
+                q.push(current - 1);
             }
 
-            if ((j + 1 < n) && (mp.find(arr[j + 1]) != mp.end())) {
-                if (j + 1 == n - 1) return step;
-                q.push(j + 1);
+            if ((current + 1 < n) && (mp.find(arr[current + 1]) != mp.end())) { // if index + 1 is not outside of vector, and value is in the map, push the value to queue
+                if (current + 1 == n - 1) return step;
+                q.push(current + 1);
             }
 
-            if (mp.find(arr[j]) != mp.end()) {
-                for (int k : mp[arr[j]]) {
-                    if (k != j) {
-                        if (k == n - 1) return step;
-                        q.push(k);
+            if (mp.find(arr[current]) != mp.end()) { // if the map contains the current value:
+                for (int k : mp[arr[current]]) { // iterates through the vector of current's value
+                    if (k != current) { // if the value in the vec is not equal to current (skips current index)
+                        if (k == n - 1) return step; // if the index is equal to the end (meaning you can jump from current index to the end), return steps taken
+                        q.push(k); // if not, add k to the queue
                     }
                 }
             }
 
-            mp.erase(arr[j]);
+            mp.erase(arr[current]); // deletes current value so you don't backtrack, and at most, it iterates over the vector once
         }
     }
 
