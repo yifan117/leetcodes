fc2464dd76c4e762b97110f2399687aeea1ab5a6
yifan117 <yifan.qiu17@gmail.com>
2023-03-05 15:19:59 +1030

    completed

--- partition_labels.cpp
+++ partition_labels.cpp
@@ -31,32 +31,29 @@ std::vector<int> partitionLabels(std::string s) { // O(N) time, O(N) space
     return ans;
 }
 
-std::vector<int> partitionLabelsOptimal(std::string s) {
-    std::vector<int> active (26, 0);
-    std::vector<int> all (26, 0);
+std::vector<int> partitionLabelsOptimal(std::string s) { // O(N) time, O(1) space
+    std::vector<int> active (26, 0); // vector we'll be checking if a value is active or not with
+    std::vector<int> all (26, 0); // holds frequency of all vals
     std::vector<int> res;
     int count = 0;
-    bool partition = true;
 
-    for (char val : s) {
+    for (char val : s) { // populates frequency table
         all[val - 'a']++;
     }
 
     for (char val : s) {
-        partition = true;
         count++;
-        active[val - 'a']++;
+        active[val - 'a']++; // increments active frequency
 
-        if (active[val - 'a'] == all[val - 'a']) {
-            active[val - 'a'] = 0;
+        if (active[val - 'a'] == all[val - 'a']) { // if the frequencies match (meaning all occurrences of that letter have been seen)
+            active[val - 'a'] = 0; // set active to 0
 
             for (int i = 0; i < active.size(); i++) {
-                if (active[i] != 0) {
-                    partition = false;
+                if (active[i] != 0) { // if a value in active is still active, break
                     break;
                 }
 
-                if (partition && (i == active.size() - 1)) {
+                if (i == active.size() - 1) { // if you've reached the end of the vector without breaking, it means all values are 0's and you can push count to the answer vector
                     res.push_back(count);
                     count = 0;
                 }
